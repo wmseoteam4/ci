18461d6a9ca16622593ef4565009703297f1b653
wmseoteam4 <wm.seo.team4@gmail.com>
2015-03-23 16:34:56 -0400

    first commit
    1st commit

---
+++ application/models/signup.php
@@ -0,0 +1,17 @@
+<?php
+if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+class Signup extends CI_Model {
+
+    var $title   = '';
+    var $content = '';
+    var $date    = '';
+
+    function __construct()
+    {
+        // Call the Model constructor
+        parent::__construct();
+    }
+
+   }
+   
+   ?>
